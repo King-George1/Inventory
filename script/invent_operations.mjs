816d784c6e2fde7e4e926419c5b60d800f523369
King George <george.mensah@amalitech.org>
2021-06-19 15:56:14 +0000

    I moved the lines of the code that update the maxProdID into getTotalCartAndSubCatNum() function

--- script/invent_operations.mjs
+++ script/invent_operations.mjs
@@ -3,6 +3,26 @@ let maxProductID = 1;
 let totalItemQuantity = 0;
 let subcategories = {};
 
+const getTotalCartAndSubCatNum = () => {
+  totalItemQuantity = 0;
+  Object.keys(inventory).forEach((category) => {
+    let ref = inventory[category];
+    let subcat = Object.keys(ref);
+    subcat.forEach((subcatName) => {
+      let subcatNumber = 0;
+      ref[subcatName].products.forEach((y) => {
+        y.productID > maxProductID
+          ? (maxProductID = y.productID)
+          : (maxProductID = maxProductID);
+
+        totalItemQuantity += y.quantity;
+        subcatNumber += y.quantity;
+      });
+      subcategories[subcatName] = subcatNumber;
+    });
+  });
+};
+
 //Function responsible for adding new Items to the inventory;
 const addItem = (
   cat_name,
@@ -13,59 +33,34 @@ const addItem = (
   prodDescription
 ) => {
   let newItem = {
-    productID: maxProductID,
+    productID: maxProductID + 1,
     productName: prodName,
     quantity: prodQuantity,
     price_per_one: prodPrice,
     description: prodDescription,
   };
   inventory[cat_name][subcat_name].products.push(newItem);
-  maxProductID++;
+  //   maxProductID++;
+
+  //Here the function will update the items quantity and the subcategory items quantity
+  getTotalCartAndSubCatNum();
 };
 
-//Getting and setting the maximum productID from the product Storage
-Object.keys(inventory).forEach((subcat) => {
-  let miniCart = Object.keys(inventory[subcat]);
-  miniCart.forEach((product) => {
-    inventory[subcat][product]["products"].forEach((singleProduct) => {
-      singleProduct.productID > maxProductID
-        ? (maxProductID = singleProduct.productID)
-        : (maxProductID = maxProductID);
-    });
-  });
-});
+//This function will get total number of items, the subcategories and their number and sets
+//the maxProductID. This will function will be called in the window.onload.
+getTotalCartAndSubCatNum();
 
-//Test For the addItem() method
+//TEST FOR THE getTotalCartAndSubCatNum and test for addItem() method
 //RESULT: FUNCTION PASSED TEST
-// console.log(maxProductID);
-// addItem("phone","Pixel","KingGeorge 6", 10, 2222, {});
-// console.log(inventory['phone']['Pixel']);
-// console.log(maxProductID);
-
-const getTotalCartAndSubCatNum = () => {
-  totalItemQuantity = 0;
-  Object.keys(inventory).forEach((category) => {
-    let ref = inventory[category];
-    let subcat = Object.keys(ref);
-    subcat.forEach((subcatName) => {
-      let subcatNumber = 0;
-      ref[subcatName].products.forEach((y) => {
-        totalItemQuantity += y.quantity;
-        subcatNumber += y.quantity;
-      });
-      subcategories[subcatName] = subcatNumber;
-    });
-  });
-};
-
-//TEST FOR THE getTotalCartAndSubCatNum
-//RESULT: FUNCTION PASSED TEST
-// getTotalCartAndSubCatNum();
-// console.log(totalItemQuantity);
+// console.log("Total number of items before any action: " + totalItemQuantity);
+// console.log("Subcategory items before insertion: ");
 // console.log(subcategories);
 // console.log("Maximum product ID before insertion: " + maxProductID);
 // addItem("phone", "Pixel", "KingGeorge 6", 1, 2222, {});
+// addItem("phone", "Galaxy", "Galaxy 999", 10, 3333, {});
 // console.log("Maximum product ID after insertion: " + maxProductID);
-// getTotalCartAndSubCatNum();
-// console.log(totalItemQuantity);
+// console.log("Subcategory items after insertion: ");
 // console.log(subcategories);
+// console.log("Total number of items before any action: " + totalItemQuantity);
+
+const updateItemQuantity = (itemID, newQuantity) => {};
