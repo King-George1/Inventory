658c381c0723f3121959a3d0c8b5db58ead98e4c
King George <george.mensah@amalitech.org>
2021-06-19 12:45:26 +0000

    I added the addItem function which adds item(s) of specified category and subcategory to inventory list. I also cleaned the inventory data.

--- script/invent_operations.mjs
+++ script/invent_operations.mjs
@@ -1,2 +1,41 @@
 import { inventory } from "./inventory.mjs";
-console.log(inventory);
+let maxProductID = 1;
+
+//Function responsible for adding new Items to the inventory;
+const addItem = (
+  cat_name,
+  subcat_name,
+  prodName,
+  prodQuantity,
+  prodPrice,
+  prodDescription
+) => {
+  let newItem = {
+    productID: maxProductID,
+    productName: prodName,
+    quantity: prodQuantity,
+    price_per_one: prodPrice,
+    description: prodDescription,
+  };
+  inventory[cat_name][subcat_name].products.push(newItem);
+  maxProductID++;
+};
+
+//Getting and setting the maximum productID from the product Storage
+Object.keys(inventory).forEach((subcat) => {
+  let miniCart = Object.keys(inventory[subcat]);
+  miniCart.forEach((product) => {
+    inventory[subcat][product]["products"].forEach((singleProduct) => {
+      singleProduct.productID > maxProductID
+        ? (maxProductID = singleProduct.productID)
+        : (maxProductID = maxProductID);
+    });
+  });
+});
+
+//Test For the addItem() method
+//RESULT: FUNCTION PASSED TEST
+// console.log(maxProductID);
+// addItem("phone","Pixel","KingGeorge 6", 10, 2222, {});
+// console.log(inventory['phone']['Pixel']);
+// console.log(maxProductID);
